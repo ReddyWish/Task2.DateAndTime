27118460ca22290f7328ac65ace9644f7b51bde6
Ilya <ilya.shuvatov1@gmail.com>
2022-11-22 10:54:04 +0100

    second

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function getDaysBeforeBirthday(nextBirthdayDate) {
-    nextBirthdayDate = new Date(2023, 5, 12)
-    let now = new Date()
-    let daysTillBirthday = Math.round((nextBirthdayDate - now)/1000/60/60/24)
-    return daysTillBirthday
+function convert(ms) {
+    let days = Math.round(ms/1000/60/60/24)
+    return days
 }
 
-console.log(getDaysBeforeBirthday('через сколько мой день рождения?'))
+
+function getDaysBeforeBirthday(nextBirthdayDate, callback) {
+    nextBirthdayDate = new Date(2023, 5, 12)
+    let now = new Date()
+    let timeTillBirthday = nextBirthdayDate - now
+    let days = convert(timeTillBirthday)
+    return days
+
+}
+
+console.log(getDaysBeforeBirthday('через сколько мой день рождения?', convert))
